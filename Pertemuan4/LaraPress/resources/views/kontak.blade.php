7ef2208bb34511b1c1b12ac7ca602d396c7bab13
zahrtsa <ev.tsabitah@gmail.com>
2025-10-17 14:34:23 +0700

    [Feat]:Pertemuan4

---
+++ Pertemuan4/LaraPress/resources/views/kontak.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Kontak</title>
+</head>
+<body>
+    <h1>Hubungi Kami</h1>
+    <p>Email: zahrtsa4523145@univpancasila.ac.id</p>
+    <p>Telepon: +62 896-7832-1276</p>
+
+    <a href="/">Kembali ke Beranda</a>
+</body>
+</html>
---
+++ Pertemuan4/LaraPress/resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+ <title>Selamat Datang di LaraPress</title>
+</head>
+<body>
+ <h1>Selamat Datang di Blog LaraPress</h1>
+ <p>Ini adalah halaman utama dari aplikasi blog kita.</p>
+ <a href="/tentang-kami">Lihat Halaman Tentang Kami</a>
+</body>
+</html>
